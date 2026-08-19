4db5e7f3d8de64c16c9565758eb1bf976265556c
loboxter <lobotrxtq@gmail.com>
2026-08-18 19:51:17 -0600

    Calculadora

---
+++ calculadora_compras.js
@@ -0,0 +1,12 @@
+let arroz = 1200;
+let frijoles = 900;
+let cafee = 2500;
+let subTotal = arroz + frijoles + cafee;
+let envio = 500;
+let total = subTotal + envio;
+
+console.log("El subtotal de la compra es: " + subTotal);
+console.log("El total a pagar con envio es: " + total);
+
+let sobrante = 20 % 3;
+console.log("El sobrante de repartir 20 entre 3 amigos es: " + sobrante);
