72c3fe3553357d7347eaae1f8b894c2c4620ac07
loboxter <lobotrxtq@gmail.com>
2026-08-18 18:12:39 -0600

    Precio

---
+++ precios.js
@@ -0,0 +1,8 @@
+let precioProducto = 120;
+let esClienteFrecuente = false;
+
+if (precioProducto >= 100 || esClienteFrecuente === true) {
+    console.log('Se lleva un descuento especial.');
+} else {
+    console.log('se lleva precio normal.');
+}
