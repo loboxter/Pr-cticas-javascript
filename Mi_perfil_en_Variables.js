b27da5c4306fd7c153e4774a08df3f808c813240
loboxter <lobotrxtq@gmail.com>
2026-08-18 19:06:04 -0600

    Practiqué con variables y console.log en JavaScript

---
+++ Mi_perfil_en_Variables.js
@@ -0,0 +1,4 @@
+let Nombre = "Sebastian";
+let Edad = 28;
+let estaEstudiando = true;
+console.log("Hola, me llamo " + Nombre + ", tengo " + Edad + " años y es verdad que estoy estudiando: " + estaEstudiando);
