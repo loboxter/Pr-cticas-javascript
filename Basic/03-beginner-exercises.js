bc9797897164b5fc2d046acd88cc95c2419b8e52
loboxter <lobotrxtq@gmail.com>
2026-08-21 16:49:13 -0600

    Terminé los ejercicios de operadores en JavaScript

---
+++ Basic/03-beginner-exercises.js
@@ -0,0 +1,64 @@
+// Hola este es mi comentario en una línea.
+
+
+/*Hola este es
+mi comentario en 
+varias líneas*/
+
+
+let saludo = ('Hola')
+let numero = 5
+let booleanos = true
+let Undefined = undefined
+let Null = null
+let Symbol = ("Mi símbolo")
+let BigInt = 8945827283723827238952527535892552n
+
+
+console.log(saludo)
+console.log(numero)
+console.log(booleanos)
+console.log(undefined)
+console.log(null)
+console.log(Symbol)
+console.log(BigInt)
+
+console.log(typeof saludo)
+console.log(typeof numero)
+console.log(typeof booleanos)
+console.log(typeof undefined)
+console.log(typeof Null)
+console.log(typeof Symbol)
+console.log(typeof BigInt)
+
+saludo = 'Adiós'
+numero = 6
+booleanos = false
+Undefined = undefined
+Null = null
+Symbol = ("Sebas símbolo")
+BigInt = 2723892739823278932278n
+
+saludo = 10
+numero = "Hola"
+booleanos = null
+Undefined = undefined
+Null = null
+Symbol = ("234534534523984239582n")
+BigInt = 23822325253523147825892783424923758283257n;
+
+const Saludo = ('Hola!')
+const Número = 87
+const Booleanos = true
+const UNdefined = undefined
+const nuLL = null
+const SYmbol = ('Buenas tardes')
+const Bigint = BigInt
+
+Saludo = ("Buenas")
+Número = 100
+Booleanos = false
+UNdefined = undefined
+nuLL = null
+SYmbol = ('Hola amigos')
+Bigint = 9843374587394537357894358n
---
+++ Basic/05-operator-exercises.js
@@ -0,0 +1,50 @@
+//1-Operadores aritméticos
+
+let Suma = 2+2
+let Resta = 1-1
+let Multiplicación = 2*2
+let División = 10/5
+let Módulo = 20%3
+let Sumando = 4
+let Restando = 2
+let rojo = 5
+let verde = 20
+
+//2-Operadores asignación
+Suma = 4
+Resta += 2
+Multiplicación -= 2
+División *= 2
+Módulo /= 2
+Sumando %= 2
+Restando **= 2
+
+
+//3- 5 comparaciones verdades con diferentes operadores de comparación
+console.log(2 > 1)
+console.log(2 < 5)
+console.log(Sumando != Restando)
+console.log(Multiplicación === Sumando)
+console.log(Suma != División )
+
+//4- 5 comparaciones falsas con diferentes operadores de comparación
+console.log(2 < 1)
+console.log(2 > 9)
+console.log(Sumando == Restando)
+console.log(Suma === Resta)
+console.log(rojo == verde)
+
+//5- utuliza el operador lógico and(&&).
+console.log((Sumando < Restando) && (Multiplicación > Sumando))
+
+//6- usa el operador logico or(||).
+console.log((2 > 1) || (Restando > Sumando))
+
+//7-combina ambos operadores lógicos
+console.log((2 >1) && (3 < 5) || (Restando == 10))
+
+//8-Añade alguna negación (!).
+console.log(!(Suma == Restando))
+
+//9-Utiliza el operador ternario.
+console.log(2 > 1 ? "Correcto" : "Incorrecto")
